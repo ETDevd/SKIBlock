64512dedb116b2c217e008e4e4b7dad2f4e4c6e3
Pixl8 <68357832+pixl8dev@users.noreply.github.com>
2025-04-11 14:18:09 -0400

    merge et's and fix voidgen

--- src/main/java/com/pixl8/skiblock/VoidGenerator.java
+++ src/main/java/com/pixl8/skiblock/VoidGenerator.java
@@ -1,24 +1,33 @@
 package com.pixl8.skiblock;
 
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.generator.ChunkGenerator;
-import org.bukkit.generator.WorldInfo;
 
 import java.util.Random;
 
 public class VoidGenerator extends ChunkGenerator {
+    private static final int SPAWN_HEIGHT = 64;
+
     @Override
     public ChunkGenerator.ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, ChunkGenerator.BiomeGrid biome) {
         ChunkGenerator.ChunkData chunkData = createChunkData(world);
         
-        // Fill the chunk with air
+        // Set all blocks to air
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
-                for (int y = 0; y < 256; y++) {
+                for (int y = 0; y < world.getMaxHeight(); y++) {
                     chunkData.setBlock(x, y, z, Material.AIR);
                 }
+            }
+        }
+        
+        // Set biome to ocean for the entire chunk
+        for (int x = 0; x < 16; x++) {
+            for (int z = 0; z < 16; z++) {
+                biome.setBiome(x, z, org.bukkit.block.Biome.OCEAN);
             }
         }
         
@@ -32,6 +41,11 @@ public class VoidGenerator extends ChunkGenerator {
 
     @Override
     public Location getFixedSpawnLocation(World world, Random random) {
-        return new Location(world, 0, 64, 0);
+        return new Location(world, 0, SPAWN_HEIGHT, 0);
+    }
+
+    @Override
+    public String toString() {
+        return "VoidGenerator";
     }
 }
